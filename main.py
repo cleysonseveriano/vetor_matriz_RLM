0ef242d33bee3681aa90ae5b814ff06d936b33fb
cleysonseveriano <cleyson.tab@gmail.com>
2022-12-03 08:24:45 -0300

    Segunda questão

--- main.py
+++ main.py
@@ -68,25 +68,12 @@ def questao_2(lA,cA,lB,cB):
         for j in range(len(B[i])):
             B[i][j] = int(random()*11)
 
-    print(A)
-    print(B)
-
-    x = int(input("Digite x: "))
-    y = int(input("Digite y: "))
-
-    for i in range(len(A)):
-        for j in range(len(A[i])):
-            A[i][j] = A[i][j] * x
-
     print('Matriz A')
     for i in range(len(A)):
         for j in range(len(A[i])):
             print(A[i][j], end='    ')
         print()
     print()
-    for i in range(len(B)):
-        for j in range(len(B[i])):
-            B[i][j] = B[i][j] * y
 
     print('Matriz B')
     for i in range(len(B)):
@@ -106,5 +93,19 @@ def questao_2(lA,cA,lB,cB):
     for i in range(len(C)):
         for j in range(len(C[i])):
             print(C[i][j], end='    ')
+        print()
+
+    D = [None]*cB
+    for i in range(len(B)):
+        D[i] = [None]*lB
+
+    for i in range(len(B)):
+        for j in range(len(B[i])):
+            D[i][j] = B[j][i]
+    
+    print("Matriz D")
+    for i in range(len(D)):
+        for j in range(len(D[i])):
+            print(D[i][j], end='    ')
         print()
 questao_2(lA,cA,lB,cB)    
